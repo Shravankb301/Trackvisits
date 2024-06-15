f7eea68ba4a9353231fb2b348fab1bea6f1b475f
Shravan <shravankb301@gmail.com>
2024-06-14 21:41:05 -0500

    authentication needs fix

--- FrontEnd/my-app/app/page.tsx
+++ FrontEnd/my-app/app/page.tsx
@@ -1,16 +1,21 @@
+'use client';
 import Image from "next/image";
-
+import { signOut, useSession } from "next-auth/react";
 import router from 'next/router'
 import Link from "next/link"
 import React from "react";
 
 
 export default function Home() {
-    //move to a differnt page on click of the button  
-    
-  return (
-    
+  //move to a differnt page on click of the button  
+  const session = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/signin');
+    },
+  });
 
+  return (
     <div>
       {/* Navbar */}
       <div data-theme="dark" className="navbar shadow-lg bg-base-200 text-neutral-content">
@@ -34,14 +39,19 @@ export default function Home() {
             <h1 className="text-5xl font-bold">SHOW YOUR USER THE BEST YOU HAVE</h1>
             <p className="py-6">One script will bring increase the average time a user spends on your site</p>
             <Link href="/LogIn">
-            <button className="btn btn-primary">Get Started</button>
+              <button className="btn btn-primary">Get Started</button>
             </Link>
           </div>
         </div>
       </div>
-    </div>
+    {/* New login style */}
+      <div className="p-8">
+        <div className="text-white">{session?.data?.user?.email}</div>
+        <button className="text-white" onClick={() => signOut()} > logout</button>
 
-    
-  );
+      </div>
+      </div>
+      );
 }
-  
+
+Home.requireAuth = true
